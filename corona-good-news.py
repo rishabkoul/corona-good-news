18e449b88384b9b51fc43f82171085678584d664
Rishab Koul <rishabkoul2001@gmail.com>
2020-08-29 17:08:50 +0530

    'done'

--- corona-good-news.py
+++ corona-good-news.py
@@ -8,6 +8,7 @@ newsapi = NewsApiClient(api_key='2fab1663f88044baa813218a1b9189da')
 
 # /v2/top-headlines
 all_articles = newsapi.get_everything(q='covid',language='en',page_size=100)
+top = newsapi.get_top_headlines(q='covid',language='en',page_size=100)
 
 def main():
     st.write("""
@@ -15,6 +16,16 @@ def main():
 
 This app shows top good news headlines abput Covid-19.
 """)
+
+    for i in range(0,len(top["articles"])):
+        edu=TextBlob(str(top["articles"][i]["content"]))
+        x=edu.sentiment.polarity
+        if x>=0.3 and x<=1:
+            st.write(x)
+            st.header(top["articles"][i]["title"])
+            st.markdown("<img src='"+top["articles"][i]["urlToImage"]+"' style='width:100%;height:auto;'/>", unsafe_allow_html=True)
+            st.markdown("<p style='margin-top:20px;'><a href='"+top["articles"][i]["url"]+"' style='font-size:1.3rem;'>"+top["articles"][i]["description"]+"</a></p>", unsafe_allow_html=True)
+
     for i in range(0,len(all_articles["articles"])):
         edu=TextBlob(str(all_articles["articles"][i]["content"]))
         x=edu.sentiment.polarity
